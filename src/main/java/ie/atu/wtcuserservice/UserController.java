c001194ea1a4fc256879b70582fcd1bd6aa06cb3
Denis Costello <G00396309@atu.ie>
2023-12-07 18:58:34 +0000

    Created create user function in controller

--- src/main/java/ie/atu/wtcuserservice/UserController.java
+++ src/main/java/ie/atu/wtcuserservice/UserController.java
@@ -1,7 +1,17 @@
 package ie.atu.wtcuserservice;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    @PostMapping("/user/createUser")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createUser(@Valid @RequestBody User user){
+
+    }
 }
