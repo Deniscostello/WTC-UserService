d19e5239d89e7369b42ba0e4310ee0259b5eb98b
Denis Costello <G00396309@atu.ie>
2023-12-07 18:53:50 +0000

    First commit

---
+++ src/main/java/ie/atu/wtcuserservice/WtcUserServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.wtcuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtcUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WtcUserServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/wtcuserservice/WtcUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.wtcuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtcUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
