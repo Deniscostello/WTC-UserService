abfb798986a746d9d892e6f6bf6eae1fc85fbcbd
Denis Costello <G00396309@atu.ie>
2023-12-07 18:59:12 +0000

    Added user class

---
+++ src/main/java/ie/atu/wtcuserservice/User.java
@@ -0,0 +1,10 @@
+package ie.atu.wtcuserservice;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class User {
+}
