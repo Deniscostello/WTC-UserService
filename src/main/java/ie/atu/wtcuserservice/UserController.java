bf2400436a66844d964e8bbea7f770dad2dabc54
Denis Costello <G00396309@atu.ie>
2023-12-07 19:01:43 +0000

    imported validation  class

--- src/main/java/ie/atu/wtcuserservice/UserController.java
+++ src/main/java/ie/atu/wtcuserservice/UserController.java
@@ -1,5 +1,6 @@
 package ie.atu.wtcuserservice;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
