dac46ab17159035282fa58bcb1971819aa378d2a
Denis Costello <G00396309@atu.ie>
2024-02-06 13:56:30 +0000

    User Service

--- src/main/java/ie/atu/wtcuserservice/UserService.java
+++ src/main/java/ie/atu/wtcuserservice/UserService.java
@@ -2,10 +2,22 @@ package ie.atu.wtcuserservice;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public void createUser(User user){
         System.out.println(user);
+    }
+
+    public List<User> getUser(){
+         List<User> userFound =userRepository.findAll();
+        return  userFound;
     }
 }
